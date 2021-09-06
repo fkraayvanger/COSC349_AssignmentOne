c84e37587bd1e951d3eab4061f07c2a29e3199dc
fkraayvanger <89968563+fkraayvanger@users.noreply.github.com>
2021-09-06 18:07:54 +1200

    Add files via upload

---
+++ dog_database.sql
@@ -0,0 +1,21 @@
+CREATE TABLE dog_options (
+       dog_name VARCHAR(10),
+       dog_price DECIMAL(13, 2),
+       dog_age INT,
+       dog_breed VARCHAR(25),
+       dog_sex CHAR(1)
+);
+
+INSERT INTO dog_options VALUES
+       ('Darla', 250, 7, 'Golden Retriever', 'F');
+INSERT INTO dog_options VALUES
+       ('Sweeny', 400, 1, 'Bichon Frise Mix', 'M');
+INSERT INTO dog_options VALUES
+       ('Caty', 260, 6, 'Unknown', 'F');
+INSERT INTO dog_options VALUES
+       ('Fido', 360.99, 3, 'Sausage Dog Mix','M');
+INSERT INTO dog_options VALUES       
+       ('Daryl', 300, 6, 'Unknown', 'M');
+INSERT INTO dog_options VALUES
+       ('Charlie', 360, 4, 'Poodle Mix', 'M');
+
